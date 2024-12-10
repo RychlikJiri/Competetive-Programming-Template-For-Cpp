ebe26ab53bb996698f89ef9f8e9f273ae303c18d
RychlikJiri <121319853+RychlikJiri@users.noreply.github.com>
2024-12-10 19:00:53 +0100

    Initialization

---
+++ TemplateForCompetetiveProgramming/TemplateForCompetetiveProgramming/Cpp Competetive Programming Template.cpp
@@ -0,0 +1,37 @@
+﻿// Cpp Competetive Programming Template.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+
+#include <iostream>
+#include <vector>        // Vectors: push_back, size, pop_back
+#include <algorithm>     // Algorithms: sort, reverse, binary_search
+#include <map>           // Maps: insert, find, erase
+#include <set>           // Sets: insert, find, erase
+#include <queue>         // Priority Queue: push, pop, top
+#include <cmath>         // Math functions: pow, sqrt, abs, fdim, fmax, fmin
+#include <string>        // String operations: length, substr, find
+#include <numeric>       // Numeric: gcd, lcm, accumulate
+#include <functional>    // Functional: greater<int> for Min-Heap
+#include <climits>       // Limits: INT_MAX, LLONG_MIN
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define endl '\n' 
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define RFOR(i, a, b) for (int i = (a); i >= (b); --i)
+#define sz(x) (int)((x).size())
+
+const int MOD = 1e9 + 7;     
+const int INF = 1e9;         
+const ll LINF = 1e18;        
+const double PI = acos(-1);  
+
+// ios::sync_with_stdio(0);   //speeds up io
+// cin.tie(0);
+// cout.tie(0);
+
+int main()
+{
+
+}
